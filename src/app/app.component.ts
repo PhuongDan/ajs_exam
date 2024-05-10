4372723ae257285c969a5b9d7a1e5f0e182f2bca
PhuongDan <dinhphuongdan@gmail.com>
2024-05-11 00:03:33 +0700

    upcode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [
+    CommonModule // Import CommonModule ở đây nếu bạn sử dụng các directive như ngFor và ngIf trong AppComponent
+  ]
 })
 export class AppComponent {
-  title = 'ajs_exam';
+  cities = [
+    { name: 'Rome', employees: [{ name: 'John Doe', position: 'Team Lead',avatar:'https://png.pngtree.com/png-clipart/20240321/original/pngtree-avatar-job-businessman-flat-portrait-of-man-png-image_14641965.png' }, { name: 'Jane Smith', position: 'Developer',avatar:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrKNwdQbVOo172aY_76IFKffFWu7h_SiuZQA&usqp=CAU' }] },
+    { name: 'Paris', employees: [{ name: 'Robert King', position: 'Team Lead',avatar:'https://png.pngtree.com/png-clipart/20240321/original/pngtree-avatar-job-businessman-flat-portrait-of-man-png-image_14641965.png' }, { name: 'Michael Suyama', position: 'Developer',avatar:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrKNwdQbVOo172aY_76IFKffFWu7h_SiuZQA&usqp=CAU'  },{ name: 'Margaret Peacook', position: 'Developer',avatar:'https://banner2.cleanpng.com/20180330/yde/kisspng-computer-icons-avatar-businessperson-company-avatar-5abe8140104dd3.1211798015224343680668.jpg' }] },
+    { name: 'London', employees: [{ name: 'David Brown', position: 'Team Lead',avatar:'https://png.pngtree.com/png-clipart/20240321/original/pngtree-avatar-job-businessman-flat-portrait-of-man-png-image_14641965.png' }, { name: 'Emma Johnson', position: 'Developer',avatar:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrKNwdQbVOo172aY_76IFKffFWu7h_SiuZQA&usqp=CAU'  }] }
+  ];
+
+  selectedCity: any;
+
+  selectCity(city: any) {
+    this.selectedCity = city;
+  }
 }
